49b05caf80e37c8659ddb1bbfb526c9ddad67d62
Valentin447 <valentin@myttk.ru>
2022-10-03 18:59:30 +0700

    Add methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿void Task34()
 {
+    // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 }
 void Task36()
@@ -10,7 +11,30 @@ void Task38()
 {
 
 }
-
+void FillArray(int[] arr)
+{
+    Random random = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(-99, 100);
+    }
+}
+void ShowArray(int[] arr)
+{
+    Console.Write("{");
+    for(int i = 0; i < arr.Length; i++)
+    {   
+        if(i + 1 !=  arr.Length)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        else
+        {
+            Console.Write(arr[i]);
+        }
+    }
+    Console.Write("}");
+}
 Task34();
 //Task36();
 //Task38();
